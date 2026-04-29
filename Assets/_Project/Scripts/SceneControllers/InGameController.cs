5bda5e97d7dc9bd1cf8e20a06e6adb21bc5bb41a
voidpeople <voidpeople@gmail.com>
2026-04-29 10:13:08 +0900

    폴더 정리중~

--- Assets/_Project/Scripts/SceneControllers/InGameController.cs
+++ Assets/_Project/Scripts/SceneControllers/InGameController.cs
@@ -8,9 +8,4 @@ public class InGameController : MonoBehaviour
         GameManager.Instance.SetGameState(GaemState.InGame);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
