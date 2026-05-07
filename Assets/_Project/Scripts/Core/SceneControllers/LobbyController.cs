873dbc20f0efbd3534671542688e6bcfdc13d150
voidpeople <voidpeople@gmail.com>
2026-05-07 16:52:00 +0900

    로비 하단 탭 버튼들 작업중~

--- Assets/_Project/Scripts/Core/SceneControllers/LobbyController.cs
+++ Assets/_Project/Scripts/Core/SceneControllers/LobbyController.cs
@@ -1,10 +1,33 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum LobbyTabType
+{
+    Stores = 0,
+    Equipment,
+    Battle,
+    Challenge,
+    Evolution
+}
 
 public class LobbyController : MonoBehaviour
 {
+    [SerializeField] private ToggleGroup _lobbyStateTabBtnGroup;
+    [SerializeField] private Toggle[] _lobbyStateTabBtns;
+
+    private LobbyTabType _currentLobbyTabType = LobbyTabType.Battle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameManager.Instance.SetGameState(GaemState.Lobby);
+
+        SetBattleState();
+    }
+
+    void SetBattleState()
+    {
+        _currentLobbyTabType = LobbyTabType.Battle;
+        _lobbyStateTabBtns[(int)_currentLobbyTabType].isOn = true;
     }
 }
---
+++ Assets/_Project/Scripts/UI/Lobby/LobbyTabModel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using R3;
+
+//MVP 패턴 - Model
+
+public class LobbyTabModel
+{
+    public ReactiveProperty<LobbyTabType> CurrentTab { get; }
+    = new ReactiveProperty<LobbyTabType>(LobbyTabType.Battle);
+
+    public void SelectTab(LobbyTabType tabType)
+        => CurrentTab.Value = tabType;
+}
