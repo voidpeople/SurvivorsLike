e41848ad4ab96cfb979fcfda8bf8b276627ec9e3
voidpeople <voidpeople@gmail.com>
2026-05-08 13:33:34 +0900

    폴더 정리중~

--- Assets/_Project/Scripts/UI/Lobby/Views/BattlePanelView.cs
+++ Assets/_Project/Scripts/UI/Lobby/Views/BattlePanelView.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +8,7 @@ namespace SurvivorsLike
     {
         [SerializeField] private Button _battleStartButton;
 
+        //BattlePanelPresenter의 OnGameStartClicked()가 구독한다.
         public event Action OnGameStartClicked;
 
         public void Init()
@@ -15,8 +16,10 @@ namespace SurvivorsLike
             _battleStartButton.onClick.AddListener(() => OnGameStartClicked?.Invoke());
         }
 
+        //플레이어의 입력을 막고자 할 때~
         public void SetInteractable(bool interactable)
         {
+            //interactable에 false을 설정하면 버튼의 입력을 막게 된다.
             _battleStartButton.interactable = interactable;
         }
 
