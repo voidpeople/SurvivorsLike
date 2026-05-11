a68fa92ee2bd8067169efa4663d1e5e5c77e8f5c
voidpeople <voidpeople@gmail.com>
2026-05-11 22:00:26 +0900

    스테이지 UI 작업중~

---
+++ Assets/_Project/Scripts/UI/Lobby/Views/StageSelectPanelView.cs
@@ -0,0 +1,14 @@
+﻿using SurvivorsLike.UI.Lobby;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectPanelView : MonoBehaviour
+{
+    [SerializeField] private StageScrollController _stageScrollCtrl;
+    [SerializeField] private Button _selectButton;
+    [SerializeField] private Button _exitButton;
+
+    private void Start()
+    {
+    }
+}
