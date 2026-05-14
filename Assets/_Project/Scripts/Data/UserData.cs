5904e5bef6091f2e64689bc4787c027dfbd82baf
voidpeople <voidpeople@gmail.com>
2026-05-14 15:14:21 +0900

    UserData을 파이어베이스에서 사용할 수 있도록 수정~

--- Assets/_Project/Scripts/Data/UserData.cs
+++ Assets/_Project/Scripts/Data/UserData.cs
@@ -1,20 +1,24 @@
-﻿using NUnit.Framework;
+﻿using Firebase.Firestore;
+using NUnit.Framework;
 using System;
 
 
 
 namespace SurvivorsLike
 {
-    [Serializable]
+    //[FirestoreData]     - "이 클래스를 Firestore와 변환할 수 있다"고 등록
+    //[FirestoreProperty] - "이 필드를 Firestore 문서의 필드로 매핑한다"고 지정
+
+    [FirestoreData]
     public class UserData
     {
-        public string userID;
-        public string nickName;
-        public int    level;
-        public int    gold;
-        public int    gem;
-        public int    selectedChapterID;    //현재 선택된 챕터의 아이디
-        public int    lastClearedChapterID; //마지막으로 클리어 한 챕터의 아이디
+        [FirestoreProperty] public string userID { get; set; }
+        [FirestoreProperty] public string nickName { get; set; }
+        [FirestoreProperty] public int level { get; set; }
+        [FirestoreProperty] public int gold { get; set; }
+        [FirestoreProperty] public int gem { get; set; }
+        [FirestoreProperty] public int selectedChapterID { get; set; }    //현재 선택된 챕터의 아이디
+        [FirestoreProperty] public int lastClearedChapterID { get; set; } //마지막으로 클리어 한 챕터의 아이디
     }
 }
 
