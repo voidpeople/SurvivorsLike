2691ec8dd4a3b4f6c903e93058e002de6237ee44
voidpeople <voidpeople@gmail.com>
2026-05-19 08:51:30 +0900

    맵 컨트롤러 추가

--- Assets/_Project/Scripts/Core/SceneControllers/InGameController.cs
+++ Assets/_Project/Scripts/Core/SceneControllers/InGameController.cs
@@ -9,6 +9,9 @@ namespace SurvivorsLike
 {
     public class InGameController : MonoBehaviour
     {
+        [Header("게임플레이")]
+        [SerializeField] private GameObject _groundObject;
+
         [Header("결과창")]
         [SerializeField] private Canvas _resultPanelCanvas;
         [SerializeField] private GameResultPanelView _resultPanelView;
@@ -31,10 +34,19 @@ namespace SurvivorsLike
             Destroy();
         }
 
-        private void Init()
+        private async UniTaskVoid Init()
         {
             CancellationToken ct = this.GetCancellationTokenOnDestroy();
+            await SetupMapAsync(GameManager.Instance.SessionData.ChapterData.mapData, ct);
+            InitResultPanelAsync(ct);
+        }
 
+        public async UniTask SetupMapAsync(MapDataSO mapData, CancellationToken ct)
+        {
+        }
+
+        private void InitResultPanelAsync(CancellationToken ct)
+        {
             _resultModel = new GameResultPanelModel();
             _resultPanelView.Init();
             _resultPresenter = new GameResultPanelPresenter(
@@ -56,7 +68,6 @@ namespace SurvivorsLike
             _resultPresenter.Hide();
             _resultPanelCanvas.gameObject.SetActive(false);
         }
-
 
         private void Destroy()
         {
