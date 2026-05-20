6430f9240a10eae65099b55216fc93fe818e1366
voidpeople <voidpeople@gmail.com>
2026-05-20 18:36:56 +0900

    플레이어 캐릭터 애니메이션 작업중~

--- Assets/_Project/Scripts/Character/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Character/Player/PlayerController.cs
@@ -1,5 +1,18 @@
-﻿using UnityEngine;
+﻿using SurvivorsLike;
+using UnityEngine;
 
+
+
+/*
+Player(루트)
+    ├─ PlayerController.cs     ← 이동, 입력, 상태
+    ├─ PlayerStats.cs          ← HP, 스탯
+    ├─ CharacterController     ← 물리 이동
+    │
+    └─ RobotKyle (자식)        ← 순수 비주얼만 담당
+         ├─ Animator
+         └─ SkinnedMeshRenderer
+*/
 
 namespace SurvivorsLike
 {
