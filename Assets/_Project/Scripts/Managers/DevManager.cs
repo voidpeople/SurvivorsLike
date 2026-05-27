e45bf6c9ed08327407d887fbd95bac9dd60e30d1
voidpeople <voidpeople@gmail.com>
2026-05-27 16:55:23 +0900

    주석 추가

--- Assets/_Project/Scripts/Managers/DevManager.cs
+++ Assets/_Project/Scripts/Managers/DevManager.cs
@@ -7,8 +7,12 @@ using UnityEngine.Rendering.Universal;
 
 namespace SurvivorsLike
 {
+    //개발용 매니저~
     public class DevManager : MonoBehaviour
     {
+        [Header("Lobby")]
+
+        [Header("InGame")]
         [SerializeField] private GameObject _managerPrefab;
         [SerializeField] private int _loadChapterId = 1;
 
@@ -23,11 +27,15 @@ namespace SurvivorsLike
             //매니저들이 없다면 매니저 프리팹 로드
             if (Object.FindFirstObjectByType<GameManager>() == null)
             {
+                //에디터에서 InGame씬 등에서 곧바로 플레이 할 경우 매니저들이 로드 안 되어 있어
+                //오류가 발생하므로 그에 대한 처리~
                 Instantiate(_managerPrefab);
             }
         }
 
-        public async UniTask PrepareInGameAsync(CancellationToken ct)
+        //Bootstrap 씬 부터 정상적인 절차로 게임을 진행하지 않고 에디터에서
+        //InGame씬을 곧바로 플레이 할 경우 그와 관련된 매니저들의 사전 작업을 실행하는 함수
+        public async UniTask PrepareInGameAsync(CancellationToken ct = default)
         {
             await DataManager.Instance.InitAsync(ct);
 
