dc0ffac1bf81f85a071bcb96c574e3f481953940
voidpeople <voidpeople@gmail.com>
2026-05-28 12:24:27 +0900

    FSM 상태 클래스들 추가

---
+++ Assets/_Project/Scripts/Character/Enemy/FSM/EnemyChaseState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+
+namespace SurvivorsLike
+{
+    public class EnemyChaseState
+    {
+
+    }
+}
---
+++ Assets/_Project/Scripts/Character/Enemy/FSM/EnemyStateBase.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+namespace SurvivorsLike
+{
+    public abstract class EnemyStateBase
+    {
+
+    }
+}
