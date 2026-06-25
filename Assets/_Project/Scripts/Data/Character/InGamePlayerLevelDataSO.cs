5fd84c0dce3c3fab04e371b22ce23cacc632a35a
voidpeople <voidpeople@gmail.com>
2026-06-25 18:25:37 +0900

    인게임 플레이어 레벨 데이터 SO클래스와 시트 다운로더 추가

---
+++ Assets/_Project/Scripts/Data/Character/InGamePlayerLevelDataSO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TriInspector;
+using UnityEngine;
+
+
+namespace SurvivorsLike
+{
+    [Serializable]
+    public class InGamePlayerLevelData
+    {
+        public int Level;
+        public int XpRequiredPerLevel;  //레벨 당 필요 경험치
+    }
+
+    //쿠나이, 드릴샷...
+    [CreateAssetMenu(fileName = "InGamePlayerLevelDataSO", menuName = "SurvivorsLike/Data/InGamePlayerLevelDataSO")]
+    public class InGamePlayerLevelDataSO : ScriptableObject
+    {
+        //[TableList]은 Tri-Inspector의 명령어
+        //역할: List<T> 를 Unity Inspector에서 테이블 형태로 표시
+        [TableList]
+        public List<InGamePlayerLevelData> LevelDataList;
+
+        public InGamePlayerLevelData GetLevelData(int level)
+        {
+            InGamePlayerLevelData levelData = LevelDataList.FirstOrDefault(d => d.Level == level);
+            if (levelData != null)
+                return levelData;
+
+            Debug.LogError($"{nameof(InGamePlayerLevelData)}::GetLevelData => InGamePlayerLevelData not found. - Level: {level}");
+            return null;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Tools/SheetDownloader/InGamePlayerLevelSheetDownloader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurvivorsLike
+{
+    public class InGamePlayerLevelSheetDownloader
+        : SingleCollectionSheetDownloader<InGamePlayerLevelDataSO, InGamePlayerLevelData>
+    {
+        protected override InGamePlayerLevelDataSO CreateSO()
+        {
+            var so = ScriptableObject.CreateInstance<InGamePlayerLevelDataSO>();
+            return so;
+        }
+
+        protected override InGamePlayerLevelData ParseRowData(
+            Dictionary<string, string> row)
+        {
+            return new InGamePlayerLevelData
+            {
+                Level = Int(row, "Level"),
+                XpRequiredPerLevel = Int(row, "XpRequiredPerLevel"),
+            };
+        }
+
+        //파싱된 LevelData 리스트를 SO의 LevelDataList 필드에 주입
+        protected override void ApplyDataList(
+            InGamePlayerLevelDataSO so,
+            List<InGamePlayerLevelData> dataList)
+            => so.LevelDataList = dataList;
+
+        protected override string GetAssetFileName(InGamePlayerLevelDataSO so)
+            => $"InGamePlayerLevelData";
+    }
+}
