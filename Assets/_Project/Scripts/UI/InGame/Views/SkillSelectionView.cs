cc64cc94dff9bb138c443a5d6f5dc5eb61bf6908
voidpeople <voidpeople@gmail.com>
2026-06-29 17:58:45 +0900

    레벨업 프리팹 수정

--- Assets/_Project/Scripts/UI/InGame/Views/SkillSelectionView.cs
+++ Assets/_Project/Scripts/UI/InGame/Views/SkillSelectionView.cs
@@ -4,6 +4,18 @@ namespace SurvivorsLike
 {
     public class SkillSelectionView : MonoBehaviour
     {
+        [SerializeField] private SkillCardView[] _cards;  // 인스펙터에서 3개 연결
 
+        public SkillCardView[] Cards => _cards;
+
+        public void Show(SkillOptionData[] options)
+        {
+            for (int i = 0; i < _cards.Length; i++)
+                _cards[i].Init(options[i]);
+
+            gameObject.SetActive(true);
+        }
+
+        public void Hide() => gameObject.SetActive(false);
     }
 }
