a1f82b45d809db22e69fa813c3ff09b199750fc3
voidpeople <voidpeople@gmail.com>
2026-07-02 21:35:54 +0900

    플레이어 캐릭터 부활창 작업중~

---
+++ Assets/_Project/Scripts/UI/InGame/Presenters/PlayerRevivePresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace SurvivorsLike
+{
+    public class PlayerRevivePresenter : IDisposable
+    {
+        private readonly PlayerReviveView _view;
+
+        public void Dispose()
+        {
+        }
+    }
+}
